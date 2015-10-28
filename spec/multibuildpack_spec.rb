b0d2808bcbcf42e2a9b2de7dc1bb835823095a15
Terence Lee <hone02@gmail.com>
2015-10-28 11:40:33 -0700

    clean up test output for multibp test

--- spec/multibuildpack_spec.rb
+++ spec/multibuildpack_spec.rb
@@ -3,7 +3,6 @@ require 'spec_helper'
 describe "Multibuildpack" do
   it "works with node" do
     Hatchet::Runner.new("node_multi", buildpack_url: "https://github.com/ddollar/heroku-buildpack-multi.git").deploy do |app|
-      puts app.output
       expect(app.output).to match("Node Version in Ruby buildpack is: v4.1.2")
     end
   end
